20c37bbfa2da5b10c06c6a770dd14d5e6ed29a4a
SanjuMaduranga <sanjulamaduranga99@gmail.com>
2024-08-04 11:55:02 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { UserButton } from '@clerk/nextjs'
+
+
+const Home = () => {
+  return (
+    <div>
+      <p>Home</p>
+
+      <UserButton />
+    </div>
+  )
+}
+
+export default Home
---
+++ app/(root)/transformations/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TransformationPage = () => {
+  return (
+    <div>
+      TransformationPage
+    </div>
+  )
+}
+
+export default TransformationPage
+
+// localhost:3000/transformations
+// localhost:3000/transformations/[id]
